3e0eeee4d16e267fe94abf6422abb2544ec394a0
Ian <il356@bath.ac.uk>
2022-04-02 22:39:39 +0100

    version 1 client.py, server.py & network.py

---
+++ client.py
@@ -0,0 +1,39 @@
+import sys
+from _thread import *
+import socket
+
+server = "138.38.242.150"
+port = 5000
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+try:
+    s.bind((server,port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("waiting for connection, Sever Started")
+
+def threaded_client(conn):
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+            
+            if not data:
+                print("disconnect")
+                break
+            else:
+                print("Received: ",reply)
+                print("Sending: ",reply)
+            
+            conn.sendall(str.encode(reply))
+        except:
+            break    
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to :",addr)
+    start_new_thread(threaded_client,(conn,))
---
+++ network.py
@@ -0,0 +1,19 @@
+import socket
+
+class Network:
+    def _init_(self):
+        self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.sever = "138.38.242.150"
+        self.port = 5000
+        self.addr = (self.server , self.port)
+        self.id = self.connect()
+        print(self.id)
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+    
+n = Network()
